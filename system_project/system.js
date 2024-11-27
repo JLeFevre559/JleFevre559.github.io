ce4f48ddfe6400e8e3b318efe5f5368256e1fd8f
jlefevre559 <jlefevre559@gmail.com>
2024-11-27 03:18:28 -0700

    Undo

--- system_project/system.js
+++ system_project/system.js
@@ -99,9 +99,10 @@ function add_planets_to_html(planets){
         
         // This line sets the scale of the planet on a logarithmic scale so that earth is original sized, and jupiter is 3x larger
         let planet_scale = (radius/6371)**0.4545
-        let earth_width = window.innerWidth*.05;
-        let planet_height = earth_width*planet_scale;
-        let planet_width = earth_width*planet_scale;
+
+        let planet_height = 100*planet_scale;
+        let planet_width = 100*planet_scale;
+
 
         // find planet offset to center the planet in its correct position on the ellipse
         let offsetX = 1/2*planet_width;
